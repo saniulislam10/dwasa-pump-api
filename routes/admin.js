5784215073c92150ff7d6ce56a68004f2e081aca
Fortunetech <saniul.client@gmail.com>
2024-12-30 01:59:53 +0600

    admin get all

--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post("/registration", controller.adminSignUp);
 router.post("/login", controller.adminLogin);
 router.post("/add", controller.add);
-router.get("/get-all", controller.getAll);
+router.post("/get-all", controller.getAll);
 router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
